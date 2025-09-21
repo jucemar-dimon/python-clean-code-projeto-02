35bfba4fd6dde970ff5cf33c8a01f0cbef0f2434
Jucemar Dimon <jucemar.dimon@gmail.com>
2025-09-21 09:37:05 -0300

    feat: implementing connection to db with sqlalchemy and pytest

---
+++ case.py
@@ -0,0 +1,10 @@
+class MinhaClasse:
+    def __enter__(self):
+        print("Entrei aqui!")
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print("Estou no exit!")
+
+
+with MinhaClasse() as mc:
+    print("Entrei no with")
---
+++ src/models/connection/db_connection_handler_test.py
@@ -0,0 +1,12 @@
+import pytest
+from .db_connection_handler import DbConnectionHandler
+
+
+@pytest.mark.skip(reason="Integration with Db")
+def test_db_connection_handler():
+    db_conn_handler = DbConnectionHandler()
+
+    assert db_conn_handler.session is None
+
+    with db_conn_handler:
+        assert db_conn_handler.session is not None
