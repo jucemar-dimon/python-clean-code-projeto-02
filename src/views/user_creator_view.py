85ae15ecd6ad0ab12971d47bc76619eecfedee8b
Jucemar Dimon <jucemar.dimon@gmail.com>
2025-09-21 18:43:46 -0300

    feat: implementing views

---
+++ src/views/user_creator_view.py
@@ -0,0 +1,18 @@
+from src.controllers.interfaces.user_creator import UserCreatorInterface
+from .http_types.http_request import HttpRequest
+from .http_types.http_response import HttpResponse
+
+
+class UserCreatorView:
+    def __init__(self, controller: UserCreatorInterface):
+        self.__controller = controller
+
+    def handle_insert_new_user(self, req: HttpRequest) -> HttpResponse:
+        try:
+            person_name = req.body["person_name"]
+            age = req.body["age"]
+            height = req.body["height"]
+            response = self.__controller.insert_new_user(person_name, age, height)
+            return HttpResponse(status_code=200, body=response)
+        except Exception as exception:
+            return HttpResponse(status_code=500, body={"error": str(exception)})
---
+++ src/views/user_finder_view.py
@@ -0,0 +1,16 @@
+from src.controllers.interfaces.user_finder import UserFinderInterface
+from .http_types.http_request import HttpRequest
+from .http_types.http_response import HttpResponse
+
+
+class UserFinderView:
+    def __init__(self, controller: UserFinderInterface):
+        self.__controller = controller
+
+    def handle_find_by_person_name(self, req: HttpRequest) -> HttpResponse:
+        try:
+            person_name = req.param["person_name"]
+            response = self.__controller.find_by_person_name(person_name)
+            return HttpResponse(status_code=200, body=response)
+        except Exception as exception:
+            return HttpResponse(status_code=500, body={"error": str(exception)})
